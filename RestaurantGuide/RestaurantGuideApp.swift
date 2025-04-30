c10d0a5860bfe262a75dd69fbfba4d00404f06bc
burakatakl <149960490+burakatakl@users.noreply.github.com>
2025-04-30 23:14:40 +0200

    Initial Commit

---
+++ RestaurantGuide/RestaurantGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantGuideApp.swift
+//  RestaurantGuide
+//
+//  Created by BURAK on 30/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RestaurantGuideTests/RestaurantGuideTests.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantGuideTests.swift
+//  RestaurantGuideTests
+//
+//  Created by BURAK on 30/04/2025.
+//
+
+import Testing
+@testable import RestaurantGuide
+
+struct RestaurantGuideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
